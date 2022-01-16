2be47445b98556364aeeacc1f65f30d3fe5af7c9
Phil McCarthy <philmcc2631@gmail.com>
2022-01-15 17:24:04 -0700

    Add attribute validations to item spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Item, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
+    it { should validate_presence_of :category }
+    it { should validate_numericality_of(:initial_stock).only_integer }
+    it { should validate_numericality_of(:initial_stock).is_greater_than_or_equal_to(0) }
   end
 
   # describe 'relationships' do
