5de5b2cfce35131f6bfb8bcf6c43c290994dc6e8
Phil McCarthy <philmcc2631@gmail.com>
2022-01-15 17:24:19 -0700

    Implement validations on item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
-  validates :name, :description, presence: true
+  validates :name, :description, :category, presence: true
+  validates_numericality_of :initial_stock, { only_integer: true, greater_than_or_equal_to: 0 }
 end
